b329204a5b18c3b49a29651dadca936b69ad8031
Mateus Simao <mateus.david@outlook.com>
2019-04-30 09:04:24 -0300

    Aplicando correção do arquivo OperacaoSpec

--- spec/OperacaoSpec.js
+++ spec/OperacaoSpec.js
@@ -1,7 +1,13 @@
+var digitarConta = function(conta){
+  for(var i = 0; i < conta.length; i++){
+    $("button:contains("+conta[i]+")").click();
+  }
+}
+
 describe("Operações", function() {
   
   beforeEach(function() {
-    
+    $("#resultado").val("")
   });
 
   afterEach(function(){
@@ -13,43 +19,49 @@ describe("Operações", function() {
     it("deve responder corretamente a soma", function(){
       var num1 = Math.random() * 10;
       var num2 = Math.random() * 10;
-      $("#resultado").val(num1+"+"+num2);
+      var conta = num1+"+"+num2;
+      digitarConta(conta)
       $("button.resultar").click();
-      expect($("#resultado").val()).toBe(num1+num2);
+      expect(parseFloat($("#resultado").val())).toEqual(num1+num2);
     });
 
     it("deve responder corretamente a subtração", function(){
       var num1 = Math.random() * 10;
       var num2 = Math.random() * 10;
-      $("#resultado").val(num1+"-"+num2);
+      var conta = num1+"-"+num2;
+      digitarConta(conta)
       $("button.resultar").click();
-      expect($("#resultado").val()).toBe(num1-num2);
+      expect(parseFloat($("#resultado").val())).toEqual(num1-num2);
     });
 
     it("deve responder corretamente a multiplicação", function(){
-      var num1 = Math.floor(Math.random() * 10);
-      var num2 = Math.floor(Math.random() * 10);
-      $("#resultado").val(num1+"*"+num2);
+      var num1 = Math.random() * 10;
+      var num2 = Math.random() * 10;
+      var conta = num1+"*"+num2;
+      digitarConta(conta)
       $("button.resultar").click();
-      expect($("#resultado").val()).toBe(num1*num2);
+      expect(parseFloat($("#resultado").val())).toEqual(num1*num2);
     });
 
     it("deve responder corretamente a multiplicação", function(){
-      var num1 = Math.floor(Math.random() * 10);
-      var num2 = Math.floor(Math.random() * 10);
-      $("#resultado").val(num1+"/"+num2);
+      var num1 = Math.random() * 10;
+      var num2 = Math.random() * 10;
+      var conta = num1+"/"+num2;
+      digitarConta(conta)
       $("button.resultar").click();
-      expect($("#resultado").val()).toBe(num1/num2);
+      expect(parseFloat($("#resultado").val())).toEqual(num1/num2);
     });
 
     it("deve colocar a mensagem 'Erro de operação' caso não tenha número a direita.", function(){
-      $("#resultado").val("5/");
+      //$("#resultado").val("5/");
+      $("button:contains(5)").click();
+      $("button:contains(/)").click();
       $("button.resultar").click();
       expect($("#resultado").val()).toBe("Erro de operação");
     });
 
     it("deve manter o número caso só haja o número da esquerda", function(){
-      $("#resultado").val("57.5");
+      digitarConta("57.5");
       $("button.resultar").click();
       expect($("#resultado").val()).toBe("57.5");
     });
@@ -62,41 +74,46 @@ describe("Operações", function() {
 
       it("Não deve adicionar operação se não houver números.", function(){
         $("button.operacao.somar").click();
-        expect($("#resultado").val()).toBe("");
+        expect($("#resultado").val().replace(/\s+/g,'')).toBe("");
       });
 
       it("deve adicionar a operação de soma se já houver número e clicar no botão soma", function(){
-        $("#resultado").val("5");
+        $("button:contains(5)").click();
         $("button.operacao.somar").click();
-        expect($("#resultado").val().substring(1)).toBe("+");
+        expect($("#resultado").val().replace(/\s+/g,'').substring(1)).toBe("+");
       });
 
       it("deve adicionar a operação de subtração se já houver número e clicar no botão subtração", function(){
-        $("#resultado").val("5");
+        $("button:contains(5)").click();
         $("button.operacao.subtrair").click();
-        expect($("#resultado").val().substring(1)).toBe("-");
+        expect($("#resultado").val().replace(/\s+/g,'').substring(1)).toBe("-");
       });
 
       it("deve adicionar a operação de multiplicação se já houver número e clicar no botão multiplicação", function(){
-        $("#resultado").val("5");
+        $("button:contains(5)").click();
         $("button.operacao.multiplicar").click();
-        expect($("#resultado").val().substring(1)).toBe("*");
+        expect($("#resultado").val().replace(/\s+/g,'').substring(1)).toBe("*");
       });
 
       it("deve adicionar a operação de divisão se já houver número e clicar no botão divisão", function(){
-        $("#resultado").val("5");
+        $("button:contains(5)").click();
         $("button.operacao.dividir").click();
-        expect($("#resultado").val().substring(1)).toBe("/");
+        expect($("#resultado").val().replace(/\s+/g,'').substring(1)).toBe("/");
       });
 
       it("não deve adicionar operação se já houver operação antes.", function(){
-        $("#resultado").val("5+");
-        $("button.operacao.dividir").click();
-        expect($("#resultado").val()).toBe("5+");
-
-        $("#resultado").val("5-3");
+        $("button:contains(5)").click();
         $("button.operacao.somar").click();
-        expect($("#resultado").val()).toBe("5-3");
+        $("button.operacao.dividir").click();
+        expect($("#resultado").val().replace(/\s+/g,'')).toBe("5+");
+        $("#resultado").val('')
+
+        $("button:contains(5)").click();
+        $("button.operacao.subtrair").click();
+        $("button:contains(3)").click();
+        $("button.operacao.somar").click();
+        expect($("#resultado").val().replace(/\s+/g,'')).toBe("5-3");
+        $("#resultado").val('')
       });
     });
 
@@ -107,34 +124,54 @@ describe("Operações", function() {
     it("Clicar no botão de números deve adicionar o número no botão (incluindo o .)", function(){
       $("button.numero").each(function(){
         $(this).click();
-        expect($("#resultado").val()).toBe($(this).text());
+        expect($("#resultado").val().replace(/\s+/g,'')).toBe($(this).text());
         $("#resultado").val("");
       });
     })
 
     it("não deve adicionar dois pontos seguidos.", function(){
       $("button.numero:contains(.)").click();
-      expect($("#resultado").val()).toBe(".");
+      expect($("#resultado").val().replace(/\s+/g,'')).toBe(".");
       $("button.numero:contains(.)").click();
-      expect($("#resultado").val()).toBe(".");
+      expect($("#resultado").val().replace(/\s+/g,'')).toBe(".");
     });
 
     it("só deve permitir um ponto por número (esquerda ou direita)", function(){
-      $("#resultado").val("25");
+      $("button:contains(2)").click();
+      $("button:contains(5)").click();
       $("button.numero:contains(.)").click();
-      expect($("#resultado").val()).toBe("25.");
+      expect($("#resultado").val().replace(/\s+/g,'')).toBe("25.");
+      $("#resultado").val("");
 
-      $("#resultado").val("25.3");
+      $("button:contains(2)").click();
+      $("button:contains(5)").click();
       $("button.numero:contains(.)").click();
-      expect($("#resultado").val()).toBe("25.3");
+      $("button:contains(3)").click();
+      $("button.numero:contains(.)").click();
+      expect($("#resultado").val().replace(/\s+/g,'')).toBe("25.3");
+      $("#resultado").val("");
 
-      $("#resultado").val("25.3-3");
+      $("button:contains(2)").click();
+      $("button:contains(5)").click();
       $("button.numero:contains(.)").click();
-      expect($("#resultado").val()).toBe("25.3-3.");
+      $("button:contains(3)").click();
+      $("button:contains(-)").click();
+      $("button:contains(3)").click();
+      $("button.numero:contains(.)").click();
+      expect($("#resultado").val().replace(/\s+/g,'')).toBe("25.3-3.");
+      $("#resultado").val("");
 
-      $("#resultado").val("25.3-3.4");
+      $("button:contains(2)").click();
+      $("button:contains(5)").click();
       $("button.numero:contains(.)").click();
-      expect($("#resultado").val()).toBe("25.3-3.4");      
+      $("button:contains(3)").click();
+      $("button:contains(-)").click();
+      $("button:contains(3)").click();
+      $("button.numero:contains(.)").click();
+      $("button:contains(4)").click();
+      $("button.numero:contains(.)").click();
+      expect($("#resultado").val().replace(/\s+/g,'')).toBe("25.3-3.4");      
+      $("#resultado").val("");
     });
 
   });
